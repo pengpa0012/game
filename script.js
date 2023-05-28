d8aa1b18e5ff979ae2650f1c62e70de59f9b49e7
Pengpa0012 <peng.ilikha@gmail.com>
2023-05-28 22:17:12 +0800

    add movement

--- script.js
+++ script.js
@@ -12,12 +12,22 @@ canvas.height = canvasDimension.y
 class Player {
   constructor(game) {
     this.game = game
+    this.colX = this.game.width * 0.5
+    this.colY = this.game.height * 0.5
+    this.colRadius = 30
+    this.x
+    this.y
   }
 
   draw(context) {
     context.beginPath()
-    context.arc(100, 100, 20, 0, Math.PI * 2)
+    context.arc(this.colX, this.colY, this.colRadius, 0, Math.PI * 2)
     context.fill()
+  }
+
+  update() {
+    this.colX = this.game.mouse.x
+    this.colY = this.game.mouse.y
   }
 }
 
@@ -27,12 +37,33 @@ class Game {
     this.width = canvas.width
     this.height = canvas.height
     this.player = new Player(this)
+    this.mouse = {
+      x: this.width * 0.5,
+      y: this.height * 0.5
+    }
+
+    window.addEventListener("keydown", (e) => {
+      if(e.key == "a") {
+        this.mouse.x -= 30 * 0.5
+      }
+      if(e.key == "d") {
+        this.mouse.x += 30 * 0.5
+      }
+    })
   }
 
   render(context) {
     this.player.draw(context)
+    this.player.update()
   }
 }
 
 const game = new Game(canvas)
-game.render(ctx)
+
+const animate = () => {
+  ctx.clearRect(0,0, canvas.width, canvas.height)
+  game.render(ctx)
+  requestAnimationFrame(animate)
+}
+
+animate()
